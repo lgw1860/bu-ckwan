be4f04bf13f48225ab0162d868436db2e8226ec4
chriswkwan <chriswkwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2008-02-18 01:00:01 +0000

    basic arrival code works

--- HW3/simulator/Control.java
+++ HW3/simulator/Control.java
@@ -4,12 +4,13 @@ public class Control {
 
 	private double Lambda = 0.0;
 	private double Ts = 0.0;
-	private int SimTime = 0;	//max time to run simulation
+	private double SimTime = 0;	//max time to run simulation
 	
 	private double time;
 	private Schedule sched;		//schedule of arrivals/departures
 	private Event currentEvent;	//current arr/dep being processed
 	
+	private int numInQueue = 0;
 	
 	public Control(double Lambda, double Ts, int SimTime)
 	{
@@ -27,14 +28,15 @@ public class Control {
 		//schedule and execute first arrival
 		sched = new Schedule(new Event("A", 3.6));
 		currentEvent = sched.getFirstEvent();
+		time = currentEvent.getTime();
 		execute(currentEvent);
 	}
 	
 	
 	public void simulate()
 	{
-		sched.add(new Event("A", 15));
-		sched.add(new Event("D", 92.6));
+		//sched.add(new Event("A", 6.7));
+		//sched.add(new Event("D", 3.7));
 		while(time < SimTime)
 		{
 			System.out.println("time: " + time);
@@ -43,15 +45,26 @@ public class Control {
 			{
 				currentEvent = currentEvent.getNext();
 				time = currentEvent.getTime();
-				execute(currentEvent);
+				
+//				currentEvent may have been scheduled for past SimTime
+				if(time < SimTime)
+				{
+					execute(currentEvent);
+				}
 			}
 			//!!!!
-			time ++;
+			//time ++;
 			//!!!!
 		}
 		
 	}
 	
+	public double randTime()
+	{
+		double time = Math.random();
+		time = ( (int)(time*100+5) ) / 100.00;
+		return time;
+	}
 	
 	/**
 	 * Based on event type
@@ -62,6 +75,19 @@ public class Control {
 		if(e.getType() == "A")
 		{
 			System.out.println("Arrival! @ " + time);
+			
+			numInQueue++;
+			if(numInQueue == 1)
+			{
+				Event myDeparture = new Event("D", time+randTime());
+				sched.add(myDeparture);
+				System.out.println("DDD: " + myDeparture);
+			}
+			//schedule next arrival
+			Event nextArrival = new Event("A", time+randTime());
+			sched.add(nextArrival);
+			System.out.println("AAA: " + nextArrival);
+			
 		}else if(e.getType() == "D")
 		{
 			System.out.println("Departure! @ " + time);
@@ -73,10 +99,10 @@ public class Control {
 	
 	public static void main(String[] args)
 	{
-		Control c = new Control(5.5, 6.8, 100);
+		Control c = new Control(5.5, 6.8, 10);
 		System.out.println(c.Lambda + " " +  c.Ts + " " + c.SimTime);
 		
 		c.simulate();
-		
+		System.out.println(c.randTime());
 	}
 }
