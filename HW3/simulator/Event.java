12dfadcd3a7ac8c171d1c09c0f628426accf4bea
chriswkwan <chriswkwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2008-02-19 05:59:48 +0000

    Commenting is done. - need to implement monitoring Event - print stuff - possibly wrap everything in Simulator.java class - command line readin? - tables of data?

--- HW3/simulator/Event.java
+++ HW3/simulator/Event.java
@@ -1,9 +1,18 @@
+/**
+ * @author Christopher Kwan  ckwan@bu.edu  U37-02-3645
+ * @name CS350 HW3 Problem 4 M/M/1 Queue Simulator
+ * @date 2-19-2008
+ * @class Event.java - Event (Arrival, Departure, Monitor) in Simulation 
+ * 			compile:	"javac Event.java"
+ * 			run:		"java Event"
+ */
+
 package simulator;
 
 public class Event {
 
 	private double time;	//time to execute
-	private String type;	//(A)rrival or (D)eparture
+	private String type;	//(A)rrival or (D)eparture or (M)onitor
 	private Event next;		//following event, based on time
 	
 	public Event(String type, double time)
@@ -39,5 +48,17 @@ public class Event {
 	public String toString()
 	{
 		return(type + " " + time);
+	}
+	
+	public static void main(String[] args)
+	{
+		Event e1 = new Event("A", 0.5);
+		System.out.println(e1.toString());
+		
+		Event e2 = new Event("D", 1.5);
+		System.out.println(e2.toString());
+		
+		Event e3 = new Event("M", 0.01);
+		System.out.println(e3.toString());
 	}
 }
