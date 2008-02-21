101820ee72d4494ffc0d8007688183a7328d1e99
chriswkwan <chriswkwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2008-02-21 00:13:47 +0000

    Added 2-3 tests

--- CS212/HW3/DStringTests/DStringTests/Tester.cs
+++ CS212/HW3/DStringTests/DStringTests/Tester.cs
@@ -8,49 +8,175 @@ namespace DynamicString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hey Justin :(");
-
-            MyString ms = new MyString();
-
-            Console.WriteLine(ms.Name);
-            ms.Name = "Ermac";
-            Console.WriteLine(ms.Name);
-
-            //DynamicString.DString ds = new DynamicString.DString();
-            DString ds = new DString();
-            ds.Add("heyguysimads");
-            Console.WriteLine( ds.ToString() );
-
-            Console.WriteLine(ds.Add(1));
-            Console.WriteLine(ds.ToString());
-
             Tester t = new Tester();
-            Console.WriteLine(t.testAdd());
+            //t.testAdd();
+            //t.testCutout();
+            t.testDecimalize();
 
             Console.ReadLine();
         }
 
-        public int testAdd()
+        public void testAdd()
         {
             //DynamicString.DString ds1 = new DynamicString.DString("goDaddy");
-            DString ds1 = new DString("goDaddy");
+            
+            //adding null
             try
             {
-                //return( ds1.Add(8) );
-                //ds1.Add(8);
-                //Console.WriteLine("Adding an integer to a DStringFixed: PASS");
-
+                DString ds1 = new DString("kangaroo");
                 ds1.Add(null);
-                Console.WriteLine("Adding a null to a DString: PASS");
+                Console.WriteLine("PASS - Adding a null to a DString: " + ds1.ToString());
             }
             catch (Exception e)
             {
-                //Console.WriteLine("Cannot add null to DString");
-                //Console.WriteLine("Adding an integer to a DString: FAIL");
-                Console.WriteLine("Adding a null to a DString: FAIL");
+                Console.WriteLine("FAIL - Adding a null to a DString");
             }
-            return 888;
+
+            //adding empty string
+            try
+            {
+                DString ds1 = new DString("kangaroo");
+                ds1.Add("");
+                Console.WriteLine("PASS - Adding an empty string to a DString: " + ds1.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("FAIL - Adding an empty string to a DString");
+            }
+
+            //adding a space
+            try
+            {
+                DString ds1 = new DString("kangaroo");
+                ds1.Add(" ");
+                Console.WriteLine("PASS - Adding a space to a DString: " + ds1.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("FAIL - Adding a space to a DString");
+            }
+
+            //adding a tab
+            try
+            {
+                DString ds1 = new DString("kangaroo");
+                ds1.Add("\t");
+                Console.WriteLine("PASS - Adding a tab to a DString: " + ds1.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("FAIL - Adding a tab to a DString");
+            }
+
+            //adding another DString
+            try
+            {
+                DString ds1 = new DString("kangaroo");
+                ds1.Add(new DString("CROCODILE"));
+                Console.WriteLine("PASS - Adding a DString to a DString: " + ds1.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("FAIL - Adding a DString to a DString");
+            }
+
+            //adding empty DString
+            try
+            {
+                DString ds1 = new DString("kangaroo");
+                ds1.Add(new DString());
+                Console.WriteLine("PASS - Adding an empty DString to a DString: " + ds1.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("FAIL - Adding an empty DString to a DString");
+            }
 
         }
+
+        /*
+         * Cutout(string, int, int)
+         */
+        public void testCutout()
+        {
+            //adding empty DString
+            try
+            {
+                DString ds1 = new DString("kangaroo");
+                string s = "kang";
+                ds1.Cutout(s,-4,1);
+                Console.WriteLine("PASS - Adding an empty DString to a DString: " + ds1.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("FAIL - Adding an empty DString to a DString");
+            }
+
+        }
+
+        public void testDecimalize()
+        {
+            //negative int
+            try
+            {
+                DString ds1 = new DString(-18);
+                ds1.Decimalize(2);
+                Console.WriteLine("PASS - Negative int: " + ds1.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("FAIL - Negative int");
+            }
+
+            //negative number decimal places
+            try
+            {
+                DString ds1 = new DString(18);
+                ds1.Decimalize(-1);
+                Console.WriteLine("PASS - negative number decimal places: " + ds1.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("FAIL - negative number decimal places");
+            }
+
+            //large int number decimal places
+            try
+            {
+                DString ds1 = new DString(18);
+                ds1.Decimalize(100000000);
+                Console.WriteLine("PASS - large int number of decimal places: " + ds1.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("FAIL - large int number of decimal places");
+            }
+
+            //num of dec places is larger than those originally in DString
+            try
+            {
+                DString ds1 = new DString(18.5);
+                ds1.Decimalize(5);
+                Console.WriteLine("PASS - num of dec places is larger than those originally in DString: " + ds1.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("FAIL - num of dec places is larger than those originally in DString");
+            }
+
+            //num of dec places is smaller than those originally in DString
+            try
+            {
+                DString ds1 = new DString(18.56789);
+                ds1.Decimalize(2);
+                Console.WriteLine("PASS - num of dec places is smaller than those originally in DString: " + ds1.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("FAIL - num of dec places is smaller than those originally in DString");
+            }
+        }
+
+
     }
 }
