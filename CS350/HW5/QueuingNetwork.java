ef4bcbcd6129ffa4d28364532afc07e9a747316a
chriswkwan <chriswkwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2008-03-17 16:20:50 +0000

    added some disk queue functionality but now cpu queue is messed up! maybe it wasnt working in the first place?

--- CS350/HW5/QueuingNetwork.java
+++ CS350/HW5/QueuingNetwork.java
@@ -6,7 +6,10 @@ public class QueuingNetwork {
 	private Schedule sched;
 	private int monitorInc = 1000;
 	private Event currentEvent;
-	private Event currentArrival; 
+	
+	private Event currentCPUArr; 
+	private Event currentDiskArr; 
+	private Event currentNetworkArr; 
 
 	private int qCPU;
 	private int qDisk;
@@ -14,10 +17,19 @@ public class QueuingNetwork {
 
 	private int numProcesses;
 	
-	private int numDepart;
-	private double sumTq;
-	private int sumQ;
-	private double sumArr;
+	private int numCPUDepart;
+	private int numDiskDepart;
+	private int numNetworkDepart;
+	
+	private double sumCPUTq;
+	private int sumCPUQ;
+	private double sumCPUArr;
+	
+	private double sumDiskTq;
+	private int sumDiskQ;
+	
+	private double sumNetworkTq;
+	private int sumNetworkQ;
 	
 	private int sumDA;
 	private int sumNA;
@@ -32,7 +44,7 @@ public class QueuingNetwork {
 	public static void main(String[] args)
 	{
 		//100 sec = 100*1000 msec
-		QueuingNetwork qn = new QueuingNetwork(100000);
+		QueuingNetwork qn = new QueuingNetwork(1000);
 		qn.simulate();
 
 		for(int i=0; i<10; i++)
@@ -73,12 +85,18 @@ public class QueuingNetwork {
 				}
 			}
 		}
-		System.out.println("ave Tq: " + sumTq/numDepart);
-		System.out.println("ave Q: " + (double)sumQ/(double)numDepart);
-		System.out.println("ave Arr: " + sumArr/numDepart);
+		System.out.println("ave CPUTq: " + sumCPUTq/numCPUDepart);
+		System.out.println("ave CPUQ: " + (double)sumCPUQ/(double)numCPUDepart);
+		System.out.println("ave CPUArr: " + sumCPUArr/numCPUDepart);
+		
+		System.out.println("ave DiskTq: " + sumDiskTq/numDiskDepart);
+		System.out.println("ave DiskQ: " + (double)sumDiskQ/(double)numDiskDepart);
+		
+		/*
 		System.out.println("E: " + (double)sumExit/(sumExit+sumDA+sumNA));
 		System.out.println("DA: " + (double)sumDA/(sumExit+sumDA+sumNA));
 		System.out.println("NA: " + (double)sumNA/(sumExit+sumDA+sumNA));
+		*/
 	}
 
 	public void execute(Event currentEvent)
@@ -106,17 +124,21 @@ public class QueuingNetwork {
 	{
 		if(currentEvent.getType() == "CA")
 		{
-			qCPU++;
+			System.out.print(currentEvent + " -> ");
+			qCPU++; //change
 			Event myDepart = null;
-			double Ts = CPUServiceTime();
-			double IAT = ProcessArrivalTime();
-			sumArr += IAT;
+			double Ts = CPUServiceTime(); //change
+			double IAT = ProcessArrivalTime(); //change
+			sumCPUArr += IAT;
 			if(qCPU == 1)
 			{
 				myDepart = new Event("CD",currentEvent.getTime()+Ts,Ts);
 				sched.add(myDepart);
-				currentArrival = currentEvent;
+				currentCPUArr = currentEvent;
 				
+			}
+			else {
+				System.out.println("WAIT in queue");
 			}
 			Event nextArrive = new Event("CA",currentEvent.getTime() + IAT);
 			sched.add(nextArrive);
@@ -126,43 +148,48 @@ public class QueuingNetwork {
 
 			if(qCPU>0)
 			{
-				numDepart++;
+				numCPUDepart++;
 				qCPU--;
-				sumQ += qCPU;
-				double Tq = currentEvent.getTime() - currentArrival.getTime();
-				sumTq += Tq;
+				sumCPUQ += qCPU;
+				double Tq = currentEvent.getTime() - currentCPUArr.getTime();
+				sumCPUTq += Tq;
 
 				//System.out.print("q: " + qCPU + "\t");
 				//System.out.print("Tq: " + Tq + "\t");
 				//System.out.print(currentArrival + "\t");
 				//System.out.println(currentEvent);
 
-				
-				
+				/*
+				//Determine destination upon departure
 				String dest = CPUDepartProb();
+				Event destEvent = null;
 				if(dest == "E")
 				{
+					destEvent = new Event("EXIT",currentEvent.getTime());
 					numProcesses++;
-					System.out.println("Exit");
+					//System.out.println("Exit");
 					sumExit++;
 				}
 				else if(dest == "D")
 				{
-					Event nextDiskArrive = new Event("DA",currentEvent.getTime());
-					sched.add(nextDiskArrive);
-					System.out.println("Disk Arrival");
+					destEvent = new Event("DA",currentEvent.getTime());
+					sched.add(destEvent);
+					//System.out.println("Disk Arrival");
 					sumDA++;
 				}
 				else if(dest == "N")
 				{
-					Event nextNetworkArrive = new Event("NA",currentEvent.getTime());
-					sched.add(nextNetworkArrive);
-					System.out.println("Network Arrival");
+					destEvent = new Event("NA",currentEvent.getTime());
+					sched.add(destEvent);
+					//System.out.println("Network Arrival");
 					sumNA++;
 				}
 				
+				System.out.println(destEvent);
+				*/
+				
 				double Ts = CPUServiceTime();
-				double actualServiceStart = max(currentEvent.getTime(),currentArrival.getTime());
+				double actualServiceStart = max(currentEvent.getTime(),currentCPUArr.getTime());
 				Event nextDepart = new Event("CD", actualServiceStart+Ts, Ts);
 				sched.add(nextDepart);
 				updateCurrentArrival("CA");
@@ -173,7 +200,57 @@ public class QueuingNetwork {
 
 	private void executeDiskEvent(Event currentEvent)
 	{
-		
+		if(currentEvent.getType() == "DA")
+		{
+			System.out.print(currentEvent + " -> ");
+
+			qDisk++;
+			Event myDepart = null;
+			double Ts = DiskServiceTime();
+			
+			if(qDisk == 1)
+			{
+				myDepart = new Event("DD",currentEvent.getTime() + Ts);
+				sched.add(myDepart);
+				currentDiskArr = currentEvent;
+			}
+			
+			//don't schedule next arrival
+			
+			//System.out.println(destEvent);
+		}else if(currentEvent.getType() == "DD")
+		{
+			if(qDisk > 0)
+			{
+				numDiskDepart++;
+				qDisk--;
+				System.out.println("\nq: " + qDisk);
+				sumDiskQ += qDisk;
+				double Tq = currentEvent.getTime() - currentDiskArr.getTime();
+				sumDiskTq += Tq;
+				
+			}
+			//determine destination upon departure
+			String dest = DiskDepartProb();
+			Event destEvent = null;
+			
+			if(dest == "C")
+			{
+				//destEvent = new Event("CA",currentEvent.getTime());
+				//sched.add(destEvent);
+			}
+			else if(dest == "N")
+			{
+				destEvent = new Event("NA",currentEvent.getTime());
+				sched.add(destEvent);;
+			}
+			
+			double Ts = DiskServiceTime();
+			double actualServiceStart = max(currentEvent.getTime(),currentDiskArr.getTime());
+			Event nextDepart = new Event("DD", actualServiceStart+Ts);
+			sched.add(nextDepart);
+			updateCurrentArrival("DA");
+		}
 	}
 	
 	private void executeNetworkEvent(Event currentEvent)
@@ -183,11 +260,12 @@ public class QueuingNetwork {
 	
 	private void executeMonitor(Event currentEvent)
 	{
+		/*
 		System.out.print("time: " + currentTime);
 		System.out.print("\tave CPUTq: " + sumTq/numDepart);
 		System.out.print("\tave CPUQ: " + (double)sumQ/(double)numDepart);
 		System.out.println("\tave CPUArr: " + sumArr/numDepart);
-		
+		*/
 		Event nextMonitor = new Event("M",currentEvent.getTime() + monitorInc);
 		sched.add(nextMonitor);
 	}
@@ -203,14 +281,32 @@ public class QueuingNetwork {
 
 	public void updateCurrentArrival(String type)
 	{
-		while(currentArrival.getNext() != null)
+		Event currentArrival = null;
+		if(type == "CA")
 		{
-			currentArrival = currentArrival.getNext();
-			if(currentArrival.getType() == type)
+			currentArrival = currentCPUArr;
+		}
+		else if(type == "DA")
+		{
+			currentArrival = currentDiskArr;
+		}
+		else if(type == "NA")
+		{
+			currentArrival = currentNetworkArr;
+		}
+		
+		if(currentArrival != null)
+		{
+			while(currentArrival.getNext() != null)
 			{
-				return;
+				currentArrival = currentArrival.getNext();
+				if(currentArrival.getType() == type)
+				{
+					return;
+				}
 			}
 		}
+		
 	}
 
 	/*
@@ -244,11 +340,11 @@ public class QueuingNetwork {
 		//System.out.println("prob: " + prob);
 		if(prob <= 0.5)
 		{
-			return ("To CPU");
+			return ("C");
 		}
 		else
 		{
-			return ("To Network");
+			return ("N");
 		}
 	}
 
