0901055e798cacb703b84ba4983670bdb8a6fbc4
chriswkwan <chriswkwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2008-03-24 07:13:03 +0000

    submitting it now

--- CS212/Lab08/Lab08/Lab08/Program.cs
+++ CS212/Lab08/Lab08/Lab08/Program.cs
@@ -8,9 +8,19 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-            Mord m = new Mord();
-            m.run();
-            
+            double[] values = { 3.0, 5.0, 6.0, 7.0, 1.0};
+            sbyte[] tieBreakValues = { 2, 3, 1, 4, 5};
+
+            Mord m = new Mord(values,tieBreakValues);
+            //m.run();
+            Console.WriteLine("\nBigGood obtains: ");
+            m.GetMord(Mord.eDirection.BigGood);
+            m.GetKord(tieBreakValues,Mord.eDirection.BigGood);
+
+            Console.WriteLine("\n\n\nLittleGood obtains: ");
+            m.GetMord(Mord.eDirection.LittleGood);
+            m.GetKord(tieBreakValues, Mord.eDirection.LittleGood);
+
             Console.ReadLine();
             
         }
