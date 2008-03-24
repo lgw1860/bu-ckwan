1a90be456e10a7c5800e6c0f79e6900756cefa8d
chriswkwan <chriswkwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2008-03-24 05:46:55 +0000

    added cheatsheet1, understandingDelegates.txt, and Lab08 except for completed Kord

---
+++ CS212/Lab08/Lab08/Lab08/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mord m = new Mord();
+            m.run();
+            
+            Console.ReadLine();
+            
+        }
+    }
+}
