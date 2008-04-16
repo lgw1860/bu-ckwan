29dd8a57847281a07dc8ccb0c6a258aa0864eda6
chriswkwan <chriswkwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2008-04-16 14:19:59 +0000

    set project initial

---
+++ trunk/CS212/Set Project/Set Project/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace SetProject
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            Console.WriteLine("Hello World");
+
+            Set<string> s = new Set<string>();
+            //string test = (string)s.Clone();
+            //Console.WriteLine(test);
+
+            //Console.WriteLine(s.Add(null));
+            s.Add("dog");
+            s.Add("cat");
+            s.Add("bird");
+
+            /*
+            foreach (string k in s)
+            {
+                Console.WriteLine(k.ToString());
+            }
+             */
+
+            Console.ReadLine();
+        }
+    }
+}
