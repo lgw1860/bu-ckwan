c7ddabef91bd36c3b5176daf602096b98736c78a
chriswkwan <chriswkwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2008-04-16 22:40:56 +0000

    basic gui design

--- CS212/Set Project/Set Project/Program.cs
+++ CS212/Set Project/Set Project/Program.cs
@@ -14,7 +14,7 @@ namespace SetProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.Run(new Form1());
 
             Console.WriteLine("Hello World");
 
