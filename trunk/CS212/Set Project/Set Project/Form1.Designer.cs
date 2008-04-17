1bef81b6f34a9237ad9fd42177fc99fd99cdeeab
chriswkwan <chriswkwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2008-04-17 15:23:54 +0000

    donezers

--- trunk/CS212/Set Project/Set Project/Form1.Designer.cs
+++ trunk/CS212/Set Project/Set Project/Form1.Designer.cs
@@ -28,28 +28,26 @@ namespace SetProject
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button4 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "Cat",
-            "Dog",
-            "Test"});
-            this.comboBox1.Location = new System.Drawing.Point(46, 156);
+            "Set<T>",
+            "SSet",
+            "SetEnumerator<T>"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 25);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(200, 21);
             this.comboBox1.TabIndex = 0;
@@ -57,7 +55,7 @@ namespace SetProject
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(294, 73);
+            this.button1.Location = new System.Drawing.Point(376, 325);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -67,85 +65,45 @@ namespace SetProject
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(294, 25);
+            this.button2.Location = new System.Drawing.Point(376, 23);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "About";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(18, 19);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(61, 17);
-            this.checkBox1.TabIndex = 3;
-            this.checkBox1.Text = "Set<T>";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.checkBox3);
-            this.groupBox1.Controls.Add(this.checkBox2);
-            this.groupBox1.Controls.Add(this.checkBox1);
-            this.groupBox1.Location = new System.Drawing.Point(46, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 4;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Select Class to Test";
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(18, 67);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(49, 17);
-            this.checkBox3.TabIndex = 5;
-            this.checkBox3.Text = "SSet";
-            this.checkBox3.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(18, 43);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(115, 17);
-            this.checkBox2.TabIndex = 4;
-            this.checkBox2.Text = "SetEnumerator<T>";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(46, 137);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.Size = new System.Drawing.Size(99, 13);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Select Method to Test:";
+            this.label1.Text = "Select Test to Run:";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(294, 154);
+            this.button3.Location = new System.Drawing.Point(239, 23);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 6;
             this.button3.Text = "Run Test";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(46, 208);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 52);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(323, 223);
+            this.richTextBox1.Size = new System.Drawing.Size(323, 267);
             this.richTextBox1.TabIndex = 7;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(147, 442);
+            this.button4.Location = new System.Drawing.Point(109, 325);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(120, 23);
             this.button4.TabIndex = 8;
@@ -153,23 +111,33 @@ namespace SetProject
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(341, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(136, 267);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 477);
+            this.ClientSize = new System.Drawing.Size(490, 356);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Project Set - Chris Kwan";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,14 +148,11 @@ namespace SetProject
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- trunk/CS212/Set Project/Set Project/Form1.cs
+++ trunk/CS212/Set Project/Set Project/Form1.cs
@@ -17,31 +17,37 @@ namespace SetProject
 {
     public partial class Form1 : Form
     {
+        SetTest tester = new SetTest();
+
         public Form1()
         {
             InitializeComponent();
+            comboBox1.Text = "";
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (comboBox1.SelectedItem == "dog")
+            if (comboBox1.SelectedIndex == 0)
             {
-                Console.WriteLine("dogg");
-                richTextBox1.Text = "Dogggg";
+                //Console.WriteLine("Set<T>");
+                richTextBox1.Text += "\nSet<T>: Click \"Run Test\" to run the test\n";
             }
-            else
+            else if (comboBox1.SelectedIndex == 1)
             {
-                Console.WriteLine("dino");
-                richTextBox1.Text = "Dingo";
-                for (int i = 0; i < 1000; i++)
-                {
-                    richTextBox1.Text += "\n" + i;
-                }
+                //Console.WriteLine("SSet");
+                richTextBox1.Text += "\nSSet: Click \"Run Test\" to run the test\n";
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                //Console.WriteLine("SetEnumerator<T>");
+                richTextBox1.Text += "\nSetEnumerator<T>: Click \"Run Test\" to run the test\n";
+            }
+
                 //richTextBox1.SelectionLength = 0;
                 richTextBox1.SelectionStart = richTextBox1.TextLength;
                 richTextBox1.ScrollToCaret();
                 //richTextBox1.Select();
-            }
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -62,6 +68,36 @@ namespace SetProject
         private void button4_Click(object sender, EventArgs e)
         {
             richTextBox1.Clear();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                richTextBox1.Text += tester.runSetTests();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                richTextBox1.Text += tester.runSSetTests();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                richTextBox1.Text += tester.runSetEnumTests();
+            }
+            richTextBox1.SelectionStart = richTextBox1.TextLength;
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "\n\nChristopher Kwan\nckwan@bu.edu\nCS212 Devlin\nProject Set\n\n";
+            richTextBox1.SelectionStart = richTextBox1.TextLength;
+            richTextBox1.ScrollToCaret();
         }
     }
 }
