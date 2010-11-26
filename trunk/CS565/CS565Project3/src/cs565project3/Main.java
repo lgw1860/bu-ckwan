8d80759622c8ad1b5b65a2a5b879ba665aae7613
ChrisWKwan <ChrisWKwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2010-11-26 19:37:41 +0000

    Added Stemmer.java and a Stemmer test in Main.

--- trunk/CS565/CS565Project3/src/cs565project3/Main.java
+++ trunk/CS565/CS565Project3/src/cs565project3/Main.java
@@ -17,6 +17,20 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world it's me!");
+
+        //Stemmer test
+        Stemmer stemmer = new Stemmer();
+        String wordsToStem[] = {"algorithm", "algorithmic", "algorithms", "Algorithms", "algo"};
+        //String wordsToStem[] = {"truck", "trucker", "truckers", "trucks", "trucking"};
+        for (int i=0; i<wordsToStem.length; i++)
+        {
+            //stem each word
+            String curWord = wordsToStem[i].toLowerCase();
+            stemmer.add(curWord.toCharArray(), curWord.length());
+            stemmer.stem();
+            System.out.println("orig: " + wordsToStem[i]);
+            System.out.println("\tstemmed: " + stemmer.toString());
+        }
     }
 
 }
