30df798bdf87555426767910876a8f7fb0f3b922
ChrisWKwan <ChrisWKwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2010-11-26 18:44:21 +0000

    Initial import into a new subdirectory in CS565 called CS565Project3.

---
+++ CS565/CS565Project3/src/cs565project3/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cs565project3;
+
+/**
+ *
+ * @author Chris
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello world");
+    }
+
+}
