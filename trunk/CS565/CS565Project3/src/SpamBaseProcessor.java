61c3ea8c8e41bc82865ad8a51f4b92777f1523c0
ChrisWKwan <ChrisWKwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2010-11-30 03:29:12 +0000

    Created SpamBaseProcessor class and added hardcoded array of SpamBase names.

---
+++ trunk/CS565/CS565Project3/src/SpamBaseProcessor.java
@@ -0,0 +1,66 @@
+/*
+ * Christopher Kwan
+ * ckwan (at) cs.bu.edu
+ * CS 565 | Terzi | Fall 2010
+ * Programming Project 3: Classifying Spam
+ */
+
+
+/**
+ *
+ * @author Chris
+ */
+public class SpamBaseProcessor {
+
+    public static String[] names = {
+        "make"         ,
+        "address"      ,
+        "all"          ,
+        "3d"           ,
+        "our"          ,
+        "over"         ,
+        "remove"       ,
+        "internet"     ,
+        "order"        ,
+        "mail"         ,
+        "receive"      ,
+        "will"         ,
+        "people"       ,
+        "report"       ,
+        "addresses"    ,
+        "free"         ,
+        "business"     ,
+        "email"        ,
+        "you"          ,
+        "credit"       ,
+        "your"         ,
+        "font"         ,
+        "000"          ,
+        "money"        ,
+        "hp"           ,
+        "hpl"          ,
+        "george"       ,
+        "650"          ,
+        "lab"          ,
+        "labs"         ,
+        "telnet"       ,
+        "857"          ,
+        "data"         ,
+        "415"          ,
+        "85"           ,
+        "technology"   ,
+        "1999"         ,
+        "parts"        ,
+        "pm"           ,
+        "direct"       ,
+        "cs"           ,
+        "meeting"      ,
+        "original"     ,
+        "project"      ,
+        "re"           ,
+        "edu"          ,
+        "table"        ,
+        "conference"   ,
+    };
+
+}
