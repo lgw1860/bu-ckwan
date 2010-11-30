7a0026e7d4b00f42c0e973f278ab49d66b0740d0
ChrisWKwan <ChrisWKwan@3254a887-6346-0410-9bca-13cabbcf55d9>
2010-11-30 03:29:12 +0000

    Created SpamBaseProcessor class and added hardcoded array of SpamBase names.

--- CS565/CS565Project3/src/SpamFilter.java
+++ CS565/CS565Project3/src/SpamFilter.java
@@ -857,42 +857,49 @@ public class SpamFilter {
      * usage: java SpamFilter <Spam folder path> <Ham folder path>
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        if(args.length == 2)
-        {
-            String spamFolderPath = args[0];
-            String hamFolderPath = args[1];
-            System.out.println("Spam folder path: " + spamFolderPath);
-            System.out.println("Ham folder path: " + hamFolderPath);
+//    public static void main(String[] args) {
+//        if(args.length == 2)
+//        {
+//            String spamFolderPath = args[0];
+//            String hamFolderPath = args[1];
+//            System.out.println("Spam folder path: " + spamFolderPath);
+//            System.out.println("Ham folder path: " + hamFolderPath);
+//
+//            //check if both folders are valid before continuing
+//            File spamFolder = new File(spamFolderPath);
+//            File hamFolder = new File(hamFolderPath);
+//            if(!spamFolder.exists() || !hamFolder.exists())
+//            {
+//                System.out.println("\nError!");
+//                if(!spamFolder.exists())
+//                {
+//                    System.out.println("\tSpam folder path invalid, please try again.");
+//                }
+//                if(!hamFolder.exists())
+//                {
+//                    System.out.println("\tHam folder path invalid, please try again.");
+//                }
+//            }
+//            else //folder paths are valid, start classifying!
+//            {
+//                System.out.println("\nPlease wait...\n");
+//                SpamFilter spamFilter = new SpamFilter();
+//                spamFilter.crossValidate(10, spamFolderPath, hamFolderPath);
+//                System.out.println("\nAll done.\n");
+//            }
+//        }
+//        else
+//        {
+//            System.out.println("Usage: java SpamFilter \"<Spam folder path>\" \"<Ham folder path>\"");
+//            System.out.println("Example: java SpamFilter \"testdata/spam\" \"testdata/ham\"");
+//        }
+//    }
 
-            //check if both folders are valid before continuing
-            File spamFolder = new File(spamFolderPath);
-            File hamFolder = new File(hamFolderPath);
-            if(!spamFolder.exists() || !hamFolder.exists())
-            {
-                System.out.println("\nError!");
-                if(!spamFolder.exists())
-                {
-                    System.out.println("\tSpam folder path invalid, please try again.");
-                }
-                if(!hamFolder.exists())
-                {
-                    System.out.println("\tHam folder path invalid, please try again.");
-                }
-            }
-            else //folder paths are valid, start classifying!
-            {
-                System.out.println("\nPlease wait...\n");
-                SpamFilter spamFilter = new SpamFilter();
-                spamFilter.crossValidate(10, spamFolderPath, hamFolderPath);
-                System.out.println("\nAll done.\n");
-            }
-        }
-        else
+    public static void main(String[] args)
+    {
+        for(int i=0; i<SpamBaseProcessor.names.length; i++)
         {
-            System.out.println("Usage: java SpamFilter \"<Spam folder path>\" \"<Ham folder path>\"");
-            System.out.println("Example: java SpamFilter \"testdata/spam\" \"testdata/ham\"");
+            System.out.println(SpamBaseProcessor.names[i]);
         }
     }
-    
 }
